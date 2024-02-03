0aa0ddef503b2e8030a1ee6f68627a78985c3206
Simran-sys <146645387+Simran-sys@users.noreply.github.com>
2024-02-03 12:49:08 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,8 @@
+// create an express server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/comments', (req, res) => {
+    res.send('This is a GET request to /comments');
+});
